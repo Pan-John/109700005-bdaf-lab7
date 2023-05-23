58f5bad3c23e7090a4a39ddfa370fa8fbe1233cf
Pan <125814787+Pan-John@users.noreply.github.com>
2023-05-23 23:58:54 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,18 +1,6 @@
 const { ethers } = require("hardhat");
 const helpers = require("@nomicfoundation/hardhat-network-helpers");
 
-//Function to add commas to a number for better readability
-function withcommas(num){
-    var result = '',counter = 0;
-    num = (num || 0).toString();
-    for(var i = num.length-1; i>=0; i--){
-        counter++;
-        result = num.charAt(i) + result;
-        if(!(counter%3)&& i!=0){result=','+result;}
-    }
-    return result;
-}
-
 async function simulate(){
     //Compound USDC v3 contract address and needed abi
     const CometAddress = '0xc3d688B66703497DAA19211EEdff47f25384cdc3';
@@ -24,7 +12,7 @@ async function simulate(){
     //USDC token address and needed abi
     const USDCAddress = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
     const USDCAbi = [
-        "function balanceOf(address account) public view returns (uint256)",
+        "function balanceOf(address account) external view returns (uint256)",
         "function approve(address spender, uint256 amount) external returns (bool)",
     ];
 
@@ -45,7 +33,7 @@ async function simulate(){
 
     //[Print] the init USDC balance in Comet contract
     const init_USDCbalance = await USDC.balanceOf(CometAddress);
-    console.log('init USDC Balance:                         ', withcommas(init_USDCbalance));
+    console.log('init USDC Balance:                         ', ethers.utils.formatUnits(init_USDCbalance.toString(),6));
 
     //Impersonate a USDC whale address as Alice
     const aliceAddress = "0x47ac0Fb4F2D84898e4D9E7b4DaB3C24507a6D503";
@@ -53,10 +41,10 @@ async function simulate(){
     const aliceImpersonatedSigner = await ethers.getSigner(aliceAddress);
     
     //Alice supply 1000 USDC to Comet (after approving Comet), then [Print] the USDC balance in Comet
-    await USDC.connect(aliceImpersonatedSigner).approve(CometAddress, 1000);
-    await Comet.connect(aliceImpersonatedSigner).supply(USDCAddress, 1000);
+    await USDC.connect(aliceImpersonatedSigner).approve(CometAddress, ethers.utils.parseUnits("1000",6));
+    await Comet.connect(aliceImpersonatedSigner).supply(USDCAddress, ethers.utils.parseUnits("1000",6));
     const balance_AfterAliceProvideLiquidity = await USDC.balanceOf(CometAddress);
-    console.log('USDC Balance after Alice provide 1000 USDC:', withcommas(balance_AfterAliceProvideLiquidity));
+    console.log('USDC Balance after Alice provide 1000 USDC:', ethers.utils.formatUnits(balance_AfterAliceProvideLiquidity.toString(),6));
     
     //Some settings up:
     //Bob can withdraw all of USDC only if he first provide enormous collateral (Here I use WETH)
@@ -73,11 +61,11 @@ async function simulate(){
     //Bob withdraw all of USDC in Comet, then [Print] the USDC balance in Comet
     await Comet.connect(bobImpersonatedSigner).withdraw(USDCAddress, balance_AfterAliceProvideLiquidity);
     const balance_AfteBobWithdrawAllUSDC = await USDC.balanceOf(CometAddress);
-    console.log('USDC Balance after Bob Withdraw all USDC:', withcommas(balance_AfteBobWithdrawAllUSDC) );
+    console.log('USDC Balance after Bob Withdraw all USDC:', ethers.utils.formatUnits(balance_AfteBobWithdrawAllUSDC.toString(),6));
 
     //[Print] Alice tries to withdraw 1000 USDC, record what happened and print those out
     console.log('When Alice tries to withdraw 1000 USDC, this will happen:');
-    try{ await Comet.connect(aliceImpersonatedSigner).withdraw(USDCAddress, 1000); }
+    try{ await Comet.connect(aliceImpersonatedSigner).withdraw(USDCAddress, ethers.utils.parseUnits("1000",6)); }
     catch(e){ err = e.message; }
     console.log(err);
 }
